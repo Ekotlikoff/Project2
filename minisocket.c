511580101c9f10e7b11f975b2d9581b5a4a5614f
Emmett Kotlikoff <ekotlikoff@dhcp-gs-401.eduroam.cornell.edu>
2014-11-08 16:52:36 -0500

    fleshed out sudo code and added some simple code (ie generating next port number for clients)

--- minisocket.c
+++ minisocket.c
@@ -16,53 +16,73 @@
 struct minisocket
 {
 	// socket number
-	// initialized = 0 (set to 1 if ack received after SYNACK (SERVER) and 1 if ack sent(client))
-	// sema for queue (for receive calls to P on and data_handle to V on) and outer sema so that multiple 
+	network_address_t remote_address;
+	int 			  remote_port;
+	// initialized = 0 (set to 1 if ack received after SYNACK (SERVER) and 1 if ack sent(client)) (0 if dying)
+	// socket_busy = 0 (set to 1 if client tried to connect to already paired server)
+	// server_waiting = sema(0) for server waiting for connection
+	// sema(0) for queue (for receive calls to P on and data_handle to V on) and outer sema(0) so that multiple 
 												   //receivers aren't manipulating queue at the same time
 	// queue for packets
-	// sequence number (when send increment)
-	// ack number (when receive increment)
-	// send semaphore (and outer sema to make sure only one person is manipulating that sema at a time)
-	// control handling function
+	int 			  seq_number; 
+	int 			  ack_number; 
+	// send semaphore(0) (and outer sema(1) to make sure only one person is manipulating that sema at a time)
+	// pointer to current control handling function
 };
 
 // big array of clients and servers
 minisocket_t ports[client_upperbound];
+int* current_client; //current client port number
 
 // type of handle control packet function, each socket has one of these
 typedef void (*handle)(mini_header_t); /* pointer to current handling function, each socket should have one eh'? */
 
+
 // SERVER
 
 void handle_SYN (mini_header_t header){ //1
-	//check that packet is MSG_SYN, keep in mind this is being done in network handler
-	//if so set_handle(port number of this socket, 2) and wakeup the thread blocking on create server
-	// and give it address and port of client so that it will send SYN_ACK with updated ack_number and retransmit...
-}
-void handle_ACK (mini_header_t header){ //2
-	//if packet is MSG_SYN again and from the same client reply again with SYNACK, if from another client send MSG_FIN back
-	//else if is MSG_ACK notify the socket by setting initialized flag and set_handle(port number of this socket, 3)
+	//MSG_SYN -> set_handle(port number of this socket, 2)
+	//			 if packet's p_seq == this ack + 1 set this socket's ack to p_seq and continue, else return?
+	//			 V(server_waiting) 
+	//			 set remote_address
+	//			 set remote_port
+	//			 server will wake up and send SYNACK with retransmitions
+	//else drop
 }
 
-void handle_control_server (mini_header_t header){ //3
-	//if MSG_SYN send back MSG_FIN to tell client that this socket is busy
-	//THIS IS NOW DATA TRANSMISSION if ack from paired socket change seq_number /ack_number 
+// SERVER AND CLIENT ARE NOW PAIRED
+void handle_control_server (mini_header_t header){ //2
+	if (initialized == 0){
+		// if packet is MSG_SYN, if it's from different addr,port then remote_address remote_port reply with MSG_FIN
+		// else if is MSG_ACK with seq == this ack notify the socket by setting initialize flag to 1
+	}
+	else //{
+		//if MSG_SYN send back MSG_FIN to tell client that this socket is busy
+		//if ack from paired socket and its seq = this ack set flag for ack received?
+		//if MSG_FIN reply with ack and if initialized flag = 1 set alarm for 15s to reset everything and set initialized to 0
 
-	//Retransmissions may also be necessary if one end indicates that it did not receive a message that was earlier sent from 
-	//the another endpoint. This condition is detected through discrepancies between the sender's sequence number and the 
-	//receiver's acknowledgement number. Refer to the slides for more information about sequence numbers and acknowledgement 
-	//numbers.
+		//Retransmissions may also be necessary if one end indicates that it did not receive a message that was earlier sent from 
+		//the another endpoint. This condition is detected through discrepancies between the sender's sequence number and the 
+		//receiver's acknowledgement number. Refer to the slides for more information about sequence numbers and acknowledgement 
+		//numbers.
+	//}
 }
+
+//
 
 // CLIENT
 
 void handle_SYNACK (mini_header_t header){ //-1
-	//check that packet is MSG_SYNACK from same server if so reply with MSG_ACK and set flag to initialized
-	//set_handle(port number of this socket, -2)       if it's a MSG_FIN set 
+	//if MSG_SYNACK -> if from same server and it seq = this ack + 1 if so set this ack = seq and reply with 
+	//MSG_ACK and set flag to initialize /set_handle(port number of this socket, -2)       
+	//if it's a MSG_FIN set socket's socket_busy flag to 1, server will be
+	//checking this flag during initialization and return SOCKET_BUSY error
 }
 
+// SERVER AND CLIENT ARE NOW PAIRED
 void handle_control_client (mini_header_t header){ //-2
-	//THIS IS NOW DATA TRANSMISION if ack from paired socket change seq_number /ack_number 
+	//if ack from paired socket if its seq = this ack set flag for ack received?
+	//if MSG_FIN reply with ack and if initialized flag = 1 set alarm for 15s to reset everything and set initialized to 0
 
 	//Retransmissions may also be necessary if one end indicates that it did not receive a message that was earlier sent from 
 	//the another endpoint. This condition is detected through discrepancies between the sender's sequence number and the 
@@ -70,7 +90,9 @@ void handle_control_client (mini_header_t header){ //-2
 	//numbers.
 }
 
-// HANDLE DATA, called by network handler if packet is a data packet
+//
+
+// BELOW ARE EXPOSED IN HEADER FOR NETWORK HANDLER TO DIRECTLY USE
 
 void handle_data (mini_header_t header){
 
@@ -79,6 +101,8 @@ void handle_data (mini_header_t header){
 	// otherwise if initialized is 1 and this is a paired socket add to queue adjust queue sema reply with 
 	// ack and adjust seq/ack number
 }
+
+
 
 // Set the socket's control handle function to specific function
 void set_handle (minisocket_t socket, int state) {
@@ -111,6 +135,11 @@ void minisocket_initialize()
  */
 minisocket_t minisocket_server_create(int port, minisocket_error *error)
 {
+	minisocket_t this_socket;
+	if (port < 0 || port >= server_upperbound) {
+		error = SOCKET_INVALIDPARAMS;
+		return NULL;
+	}
 	// create socket
 	// add to socket array
 	//
@@ -118,8 +147,9 @@ minisocket_t minisocket_server_create(int port, minisocket_error *error)
 	// 
 	// network handler wakes up this thread and supplies it with address and port of client
 
-	// send a MSG_SYNACK (and retransmit till either timeout (where the server should reset the port and go back to listening 
-	// mode, eg set handle back to handle_syn and wipe stored address and port) or initialized flag is raised)
+	// increment seq to 1 and create a MSG_SYNACK 
+	// send and retransmit till either timeout (where the server should reset the port and 
+	// go back to listening mode, eg set handle back to handle_syn and wipe stored address and port) or initialized flag is raised)
 
 	// You have to make sure that sockets are used one-to-one, i.e. one socket will be sending packets to only one other socket. 
 	// Any attempt to open a new connection to an already used socket (i.e. socket to which there is another connection already 
@@ -128,6 +158,28 @@ minisocket_t minisocket_server_create(int port, minisocket_error *error)
 	// check if server is busy and respond with busy if that's the case
 }
 
+
+// returns next available client port, -1 if none available
+int find_next_client_port() {
+    int counter = server_upperbound;
+    if (current_client == NULL){
+        current_client = (int*)malloc(sizeof(int));
+        *current_client = server_upperbound;
+    }
+    if (*current_client < client_upperbound) { //so max val is 65535
+        current_client++;
+        return *current_client-1;
+    }
+    else {
+	    for (;counter<client_upperbound;counter++){
+	        if (ports[counter] == NULL){
+	            return counter;
+	        }
+	    }
+    }	
+    printf("ERROR: no available client ports\n");
+    return -1;
+}
 
 /*
  * Initiate the communication with a remote site. When communication is
@@ -145,9 +197,23 @@ minisocket_t minisocket_server_create(int port, minisocket_error *error)
  */
 minisocket_t minisocket_client_create(network_address_t addr, int port, minisocket_error *error)
 {
-	// generate local port number, return appropriate error if none available
-
-	// send MSG_SYN 7 times until reply, if none return SOCKET_NOSERVER
+    minisocket_t this_socket;
+    // generate local port number, return appropriate error if none available
+    int this_port_number = find_next_client_port();
+    if (this_port_number == -1){
+    	error = SOCKET_NOMOREPORTS;
+    	return NULL;
+    }
+    this_socket    		                 = (minisocket_t)malloc(sizeof(minisocket));
+	// set remote addr and port
+    this_socket->remote_address = addr;
+    this_socket->remote_port    = port;
+	// set seq = 1
+	this_socket->seq_number     = 1;
+	this_socket->ack_number     = 0;
+	// create MSG_SYN
+	// send MSG_SYN 7 times until reply, if socket_busy is 1 return SOCKET_BUSY and reset remote addr, port
+	// if none after 7 return SOCKET_NOSERVER and reset remote addr and port
 
 	// if initialized flag set (eg if SYNACK replied) return minisocket
 
@@ -176,6 +242,7 @@ minisocket_t minisocket_client_create(network_address_t addr, int port, minisock
 int minisocket_send(minisocket_t socket, minimsg_t msg, int len, minisocket_error *error)
 {
 
+//if initialized = 0 then thread is unitinitialized or dying return SOCKET_SENDERROR
 //	P outer: to force one outgoing message at a time
 //		set timeout to 100ms
 //  	keep track of total length of messages sent (taking into account only partial sending because 
@@ -207,6 +274,7 @@ int minisocket_send(minisocket_t socket, minimsg_t msg, int len, minisocket_erro
  */
 int minisocket_receive(minisocket_t socket, minimsg_t msg, int max_len, minisocket_error *error)
 {
+	//if initialized = 0 then thread is unitinitialized or dying return SOCKET_RECEIVEERROR
 	//just P on the outer queue lock and the queue sema and then pop off of data queue and V outer queue lock
 }
 
@@ -217,6 +285,7 @@ int minisocket_receive(minisocket_t socket, minimsg_t msg, int max_len, minisock
  */
 void minisocket_close(minisocket_t socket)
 {
+	// Set initialized to 0 and send FIN packet to socket and retransmit waiting for an ack received back, upon ack or after 7, reset everything
 // need to notify all threads blocking on this socket with recieve
 // after fin receives/sends on this socket should fail
 }
